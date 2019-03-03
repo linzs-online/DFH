b2f8fc8d58d19d2147847225d5311d3585111d81
lzs <45315148+physics-lzs@users.noreply.github.com>
2019-03-03 15:06:29 +0800

    Add files via upload

--- CTR/control.c
+++ CTR/control.c
@@ -23,16 +23,35 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 	{
 		timeTick++;
 		if(timeTick%10 == 0)
-		{	
-
+		{
+  		 Path_All_Deal();
+   		 Angle_change();	  
+		   SpeedPID_Calc();
+			
+//			Debug_Picking();
+//     Debug_Picking();
+//			Position_YAW_PID_Calc(&Yaw_PID,YAW_Angle_Point,Angle.car,Yaw_Error_Value,20);
+//			Mecanum_Drive(0 , 30 , Yaw_PID.PWMSum);
+//			if(HCSR_U<=150)
+//			{
+//       Motor_Stop();		
+//			}
 		}
 		if(timeTick%50 == 0)
 		{
-
+     //printf ("Angle.time=%d  Angle.car=%d\n",Angle.time,Angle.car);
+      //printf("HCSR_LD=%d   PWM=%d\n",HCSR_LD,HCSR_PID.PWMSum);			
+	   //printf("HCSR_U=%d  HCSR_RD=%d  HCSR_RU=%d  HCSR_LD=%d  HCSR_LU=%d  PWM=%d\n",HCSR_U,HCSR_RD,HCSR_RU,HCSR_LD,HCSR_LU,HCSR_PID.PWMSum);
+			//printf("encoder_LU=%d  encoder_RU=%d  encoder_LD=%d  encoder_RD=%d\r\n",encoder.encoder_LU ,encoder.encoder_RU,encoder.encoder_LD,encoder.encoder_RD);
+			//printf ("HCSR_LU=%d   HCSR_LD=%d   Angle.car=%d\n",HCSR_LU,HCSR_LD,(HCSR_LU - HCSR_LD));
+			//printf("HCSR_U=%d \n ",HCSR_U);
+			//printf("flag1=%d  flag2=%d\n ",HAL_GPIO_ReadPin(GPIOA,OpenMV_RX1_Pin),HAL_GPIO_ReadPin(GPIOA,OpenMV_RX2_Pin));
 		}
 		if(timeTick%1000 == 0)
 		{
-
+      MySecond1++;
+			MySecond2++;
+			timeTick = 0;    //防止整型变量溢出计时不准
 		}
 	}
 }  
