4067d66307c55f60a2b60663a7fa4dbc94d82b64
lzs <45315148+physics-lzs@users.noreply.github.com>
2019-03-03 11:34:59 +0800

    Add files via upload

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Picking_car' 
+ * Target:  'Picking_car' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
